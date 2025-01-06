0928a1bd712ba286e8b95ca48c3f374595eed557
Martin Becke <Martin.Becke@HAW-Hamburg.de>
2025-01-06 09:54:17 +0100

    Expand test coverage

--- src/test/java/editor/SimpleEditorTest.java
+++ src/test/java/editor/SimpleEditorTest.java
@@ -1,12 +1,30 @@
 package editor;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SimpleEditorTest {
+    private SimpleEditor editor;
+
+    @BeforeEach
+    void setUp() {
+        editor = new SimpleEditor();
+    }
+
     @Test
     void editorShouldInitialize() {
-        SimpleEditor editor = new SimpleEditor();
         assertNotNull(editor);
+    }
+
+    @Test
+    void editorShouldHaveTitle() {
+        assertEquals("Simple Editor", editor.getTitle());
+    }
+
+    @Test
+    void editorShouldHaveCorrectSize() {
+        assertEquals(800, editor.getWidth());
+        assertEquals(600, editor.getHeight());
     }
 }
